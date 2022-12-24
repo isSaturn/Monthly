ea2a2d278be3b2a194f182f398cbea88fcbeb9d5
Tran Khuong <trankhuong2512001@gmail.com>
2022-12-24 17:34:50 +0700

    Update FE website Monthly Statement

---
+++ MonthlyStatement/Areas/Admin/Controllers/YearReportController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MonthlyStatement.Models;
+
+namespace MonthlyStatement.Areas.Admin.Controllers
+{
+    public class YearReportController : Controller
+    {
+        private CP25Team04Entities db = new CP25Team04Entities();
+        // GET: Admin/YearReport
+        public ActionResult Index()
+        {
+            return View(db.ReportYears.ToList());
+        }
+    }
+}
