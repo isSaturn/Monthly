fae84c7dc6f1bb37a546e5ff7b0f390a3a0453e0
isSaturn <darkruko123@gmail.com>
2022-12-28 18:57:35 +0700

    Save form report department/personal

--- MonthlyStatement/Controllers/HomeController.cs
+++ MonthlyStatement/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
-﻿using System;
+﻿using MonthlyStatement.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
 
 namespace MonthlyStatement.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private CP25Team04Entities db = new CP25Team04Entities();
+
         public ActionResult Index()
         {
+
             return View();
         }
     }
