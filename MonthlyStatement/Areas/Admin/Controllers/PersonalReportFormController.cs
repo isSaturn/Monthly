a5801a6742de0f57dbe9cc6f43585aeaa86ce635
Tran Khuong <trankhuong2512001@gmail.com>
2023-01-02 16:59:59 +0700

    Update Form report personal (FE)

---
+++ MonthlyStatement/Areas/Admin/Controllers/PersonalReportFormController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonthlyStatement.Areas.Admin.Controllers
+{
+    public class PersonalReportFormController : Controller
+    {
+        // GET: Admin/PersonalReportForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Formreport()
+        {
+            return View();
+        }
+    }
+}
