5a87b61999f2db1b259f557c535df26c4e0a6932
Tran Khuong <trankhuong2512001@gmail.com>
2023-01-03 21:18:02 +0700

    Update code FE Report Personal

--- MonthlyStatement/Areas/Admin/Controllers/PersonalReportFormController.cs
+++ MonthlyStatement/Areas/Admin/Controllers/PersonalReportFormController.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using MonthlyStatement.Models;
 
 namespace MonthlyStatement.Areas.Admin.Controllers
 {
     public class PersonalReportFormController : Controller
     {
+        private CP25Team04Entities db = new CP25Team04Entities();
         // GET: Admin/PersonalReportForm
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult Formreport()
+        {
+            return View();
+        }
+
+        public ActionResult Personal()
+        {
+            return View();
+        }
+
+        public ActionResult PersonalDetail()
         {
             return View();
         }
