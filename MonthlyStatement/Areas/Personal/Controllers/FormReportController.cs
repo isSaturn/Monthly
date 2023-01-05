0036877e9ed14f86c952b92687daf5f412ae9933
isSaturn <darkruko123@gmail.com>
2023-01-05 20:01:33 +0700

    personal report

---
+++ MonthlyStatement/Areas/Personal/Controllers/FormReportController.cs
@@ -0,0 +1,26 @@
+﻿using MonthlyStatement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonthlyStatement.Areas.Personal.Controllers
+{
+    [Authorize(Roles = "Nhân viên - Giảng viên")]
+    public class FormReportController : Controller
+    {
+        private CP25Team04Entities db = new CP25Team04Entities();
+
+        // GET: Personal/FormReport
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FormReport()
+        {
+            return View();
+        }
+    }
+}
