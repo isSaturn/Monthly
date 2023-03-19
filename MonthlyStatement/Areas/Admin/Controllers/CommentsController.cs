09d2daaee3d97fc7d9735ab4f7203afb1a244a89
Tran Khuong <trankhuong2512001@gmail.com>
2023-03-19 14:27:41 +0700

    Code Feedback User/Admin

---
+++ MonthlyStatement/Areas/Admin/Controllers/CommentsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MonthlyStatement.Models;
+
+namespace MonthlyStatement.Areas.Admin.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private CP25Team04Entities db = new CP25Team04Entities();
+
+        [HttpPost]
+        // GET: Admin/Comments
+        public ActionResult PostComment(string CommentText)
+        {
+            string emails = User.Identity.Name;
+            string userId = db.AspNetUsers.FirstOrDefault(a => a.Email.ToLower().Equals(emails.ToLower().Trim())).Id;
+            Comment c = new Comment();
+            c.comment_content = CommentText;
+            c.comment_date = DateTime.Now;
+            c.account_id = userId;
+            db.Comments.Add(c);
+            db.SaveChanges();
+            return RedirectToAction("Index", "ListReportPersonal");
+        }
+    }
+}
