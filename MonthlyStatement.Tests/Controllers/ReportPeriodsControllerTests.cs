b2a9a13278d7e1e26ef4f3562d35867436b8f7b8
TranUyenNhi <108264973+TranUyenNhi@users.noreply.github.com>
2023-04-20 15:25:30 +0700

    Create

---
+++ MonthlyStatement.Tests/Controllers/ReportPeriodsControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonthlyStatement.Areas.Admin.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportPeriodsController.Tests
+{
+    [TestClass()]
+    public class ReportPeriodsControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+       
+
+        [TestMethod()]
+        public void FormReportsTest()
+        {
+            Assert.Fail();
+        }
+
+        
+
+        [TestMethod()]
+        public void FormDepartmentDetailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FormPersonalDetailTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ MonthlyStatement.Tests/Controllers/YearReportControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonthlyStatement.Areas.Admin.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YearReport.Tests
+{
+    [TestClass()]
+    public class YearReportControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
