636f5cd51b7b5dc294a036348cc8e07fa5ff6be7
Tran Khuong <trankhuong2512001@gmail.com>
2023-04-26 21:27:45 +0700

    Fix bug Comment and Notification

--- MonthlyStatement/Areas/Admin/Controllers/CommentsController.cs
+++ MonthlyStatement/Areas/Admin/Controllers/CommentsController.cs
@@ -60,24 +60,55 @@ namespace MonthlyStatement.Areas.Admin.Controllers
 
         [HttpPost]
         // GET: Admin/Comments
-        public async Task<ActionResult> PostComment(string CommentText)
+        public async Task<ActionResult> PostCommentPersonal(string CommentText, int id)
         {
-            
             string emails = User.Identity.Name;
             string userId = db.AspNetUsers.FirstOrDefault(a => a.Email.ToLower().Equals(emails.ToLower().Trim())).Id;
+
+            string account_id = db.PersonalReports.FirstOrDefault(t => t.personal_report_id == id).account_id;
+            string mail = db.AspNetUsers.FirstOrDefault(m => m.Id == account_id).Email;
+
             Comment c = new Comment();
             c.comment_content = CommentText;
             c.comment_date = DateTime.Now;
             c.account_id = userId;
+            c.personal_report_id = id;
+            db.Comments.Add(c);
+            db.SaveChanges();
+
+            
+            await UserManager.SendEmailAsync(mail,
+                           "Thông báo bình luận từ " + userId,
+                           "Nội dung: " + c.comment_content);
+
+            return RedirectToAction("Index", "ListReportPersonal");
+        }
+
+
+        [HttpPost]
+        // GET: Admin/Comments
+        public async Task<ActionResult> PostCommentDepartment(string CommentText, int id)
+        {
+            string emails = User.Identity.Name;
+            string userId = db.AspNetUsers.FirstOrDefault(a => a.Email.ToLower().Equals(emails.ToLower().Trim())).Id;
+
+            string account_id = db.DepartmentReports.FirstOrDefault(t => t.department_report_id == id).account_id;
+            string mail = db.AspNetUsers.FirstOrDefault(m => m.Id == account_id).Email;
+
+            Comment c = new Comment();
+            c.comment_content = CommentText;
+            c.comment_date = DateTime.Now;
+            c.account_id = userId;
+            c.department_report_id = id;
             db.Comments.Add(c);
             db.SaveChanges();
 
 
-            await UserManager.SendEmailAsync(userId,
-                           "Thông báo bình luận từ " + "" + emails ,
+            await UserManager.SendEmailAsync(mail,
+                           "Thông báo bình luận từ " + userId,
                            "Nội dung: " + c.comment_content);
 
-            return RedirectToAction("Index", "ListReportPersonal");
+            return RedirectToAction("Index", "ListReportDepartment");
         }
     }
 }
--- MonthlyStatement/Areas/Personal/Controllers/CommentsPersonalController.cs
+++ MonthlyStatement/Areas/Personal/Controllers/CommentsPersonalController.cs
@@ -4,8 +4,12 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using MonthlyStatement.Areas.Admin.Controllers;
 using MonthlyStatement.Models;
 
 namespace MonthlyStatement.Areas.Personal.Controllers
@@ -14,18 +18,77 @@ namespace MonthlyStatement.Areas.Personal.Controllers
     {
         private CP25Team04Entities db = new CP25Team04Entities();
 
+        private ApplicationAccountManager _userManager;
+        private ApplicationSignInManager _signInManager;
+
+
+        public CommentsPersonalController()
+        {
+        }
+        public CommentsPersonalController(ApplicationAccountManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+        public CommentsPersonalController(ApplicationAccountManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public ApplicationAccountManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationAccountManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+
         [HttpPost]
         // GET: Personal/CommentsPersonal
-        public ActionResult PostComment(string CommentText)
+        public async Task<ActionResult> PostCommentAsync(string CommentText, int id)
         {
             string emails = User.Identity.Name;
             string userId = db.AspNetUsers.FirstOrDefault(a => a.Email.ToLower().Equals(emails.ToLower().Trim())).Id;
+
+            /*string account_id = db.PersonalReports.FirstOrDefault(t => t.personal_report_id == id).account_id;
+            string mail = db.AspNetUsers.FirstOrDefault(m => m.Id == account_id).Email;*/
+
+
+            var list_send_comment = db.Comments.Where(p => p.personal_report_id == id).ToArray();
+
+
             Comment c = new Comment();
             c.comment_content = CommentText;
             c.comment_date = DateTime.Now;
             c.account_id = userId;
+            c.personal_report_id = id;
             db.Comments.Add(c);
             db.SaveChanges();
+
+            for (int i = 0; i < list_send_comment.Length; i++)
+            {
+                await UserManager.SendEmailAsync(list_send_comment[i].account_id,
+                              "Thông báo bình luận từ " + userId,
+                              "Nội dung: " + c.comment_content);
+
+            }
             return RedirectToAction("Personal", "Report");
         }
         
